718b52deb634adc4dd8791fac19ccc60d516233b
Ghavish <ghavishsubratty05@gmail.com>
2026-09-23 15:19:13 +0400

    Resolved Package Dependencies

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logIn } from "../../firebase/authService";
+
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
